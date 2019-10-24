0a75d1eb9e9dc24d042704a542a5ce2ba8f0faf9
Holger Morch <holger.morch@here.com>
2019-10-24 15:38:28 +0200

    Delete all features that where not updated for one hour.

--- outage_to_XYZ.js
+++ outage_to_XYZ.js
@@ -7,24 +7,55 @@ async function toXYZ() {
 	const space = process.argv.length > 2 ? process.argv[2] : process.env.XYZ_SPACE;
 	const token = process.argv.length > 3 ? process.argv[3] : process.env.XYZ_TOKEN;
 
+    function checkStatus(res) {
+        if (res.ok) { // res.status >= 200 && res.status < 300
+            return res;
+        } else {
+            throw Error(`Error during upload. Status: ${res.statusCode}`);
+        }
+    };
+
+    // Upload current data
 	await fetch(`https://xyz.api.here.com/hub/spaces/${space}/features`, {
         method: 'put',
         body:    await getGeoJson(),
-        headers: { 
+        headers: {
         	'Content-Type': 'application/geo+json',
     		"Authorization": `Bearer ${token}`,
     		"Accept": "application/geo+json"
         },
     })
-    .then( res => {
-        if (res.ok) { // res.status >= 200 && res.status < 300
-            return res;
-        } else {
-            throw Error(`Error during upload. Status: ${res.statusCode}; Body: ` + res.text());
-        }
-    })
+    .then(checkStatus)
     .then(res => res.json())
-    .then(json => console.log("Data uploaded. Body: " + JSON.stringify(json)));
+    .then(json => console.log(`Data uploaded. ${json.features.length} features`));
+	
+	
+	// Search not updated features
+	var outdatedFeatureIds = await fetch(`https://xyz.api.here.com/hub/spaces/${space}/search?selection=ids&f.updatedAt%3C` + (new Date().getTime() - (1000*60*60)), {       // search for everything older 1h
+        method: 'get',
+        headers: { 
+    		"Authorization": `Bearer ${token}`,
+    		"Accept": "application/geo+json"
+        },
+    })
+    .then(checkStatus)
+    .then(res => res.json())
+    .then(json => json.features.map ( f => f.id) );
+
+	console.log(`${outdatedFeatureIds.length} outdated features found`);
+    if (outdatedFeatureIds.length > 0) {
+		// Delete not updtaed features
+	    await fetch(`https://xyz.api.here.com/hub/spaces/${space}/features?id=` + outdatedFeatureIds.join(), {
+	        method: 'delete',
+	        headers: { 
+	    		"Authorization": `Bearer ${token}`,
+	    		"Accept": "application/x-empty"
+	        },
+	    })
+	    .then(checkStatus);
+	    
+	    console.log(`Outdated features deleted.`);
+    }
 };
 
 async function handler(event, context) {
@@ -34,5 +65,6 @@ async function handler(event, context) {
 module.exports.handler = handler;
 
 if (path.basename(process.argv[1]) == path.basename(__filename)){
+    console.log("I'm called directly. Running right away");
 	toXYZ();
 };
